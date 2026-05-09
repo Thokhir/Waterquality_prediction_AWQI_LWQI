c5cad427e93eba52880006e7e85254114eee6d78
Thokhir <sktbasha22@gmail.com>
2026-05-09 11:45:14 +0530

    Update train_combined_models.py

--- train_combined_models.py
+++ train_combined_models.py
@@ -175,7 +175,7 @@ print("\n[2/6] Engineering Livestock features...")
 df_live['Time_sin'] = np.sin(2 * np.pi * df_live['Time'] / 12)
 df_live['Time_cos'] = np.cos(2 * np.pi * df_live['Time'] / 12)
 
-X_live = df_live.drop(['LWQI', 'Code', 'Time', 'Seasons'], axis=1)
+X_live = df_live.drop(['LWQI', 'Code', 'Time'], axis=1)
 y_live_reg = df_live['LWQI']
 
 X_train_live, X_test_live, y_train_live, y_test_live = train_test_split(
